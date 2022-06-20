5ea23585ee7d184a980eee92931fe05809642acb
Henrique Reis <contatohenriquereis@gmail.com>
2022-06-20 20:56:03 -0300

    Arquivo principal do bot

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on('interactionCreate', async interaction => {
+  
+  if (!interaction.isCommand()) {
+    return;
+  }
+  
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'pong') {
+    await interaction.reply('Ping!');
+  } else if (commandName === 'server') {
+    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+  } else if (commandName === 'user') {
+    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
