a9a15e9722a08d444e1dc855e71677803fbeffce
Henrique Reis <contatohenriquereis@gmail.com>
2022-06-21 19:54:03 -0300

    Path and files to command handler

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 require("dotenv").config();
 
-const { Client, Intents } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Intents, Collection } = require("discord.js");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -13,18 +27,18 @@ client.on("interactionCreate", async (interaction) => {
     return;
   }
 
-  const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
-  } else if (commandName === "server") {
-    await interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    );
-  } else if (commandName === "user") {
-    await interaction.reply(
-      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-    );
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command",
+      ephemeral: true,
+    });
   }
 });
 
